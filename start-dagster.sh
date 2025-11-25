59a6ac6fd4e6e9994025555c019112701fce7e5d
Ahmet Efe Ersoy <ahmetefeersoy@gmail.com>
2025-11-25 23:54:19 +0300

    port fixes

--- start-dagster.sh
+++ start-dagster.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+PORT=${PORT:-3000}
 dagster-daemon run &
-exec dagster-webserver -h 0.0.0.0 -p 3000 -w /opt/dagster/dagster_home/workspace.yaml
+exec dagster-webserver -h 0.0.0.0 -p $PORT -w /opt/dagster/dagster_home/workspace.yaml
